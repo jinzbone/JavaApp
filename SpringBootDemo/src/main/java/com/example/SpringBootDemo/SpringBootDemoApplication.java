4fb83c16b8f78f0e8956b30fe1e92b9ca7fca33e
e <jinzbone@gmail.com>
2020-10-27 17:48:17 +0800

    SpringBoot样例

--- SpringBootDemo/src/main/java/com/example/SpringBootDemo/SpringBootDemoApplication.java
+++ SpringBootDemo/src/main/java/com/example/SpringBootDemo/SpringBootDemoApplication.java
@@ -2,8 +2,10 @@ package com.example.SpringBootDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class SpringBootDemoApplication {
 
     public static void main(String[] args) {
