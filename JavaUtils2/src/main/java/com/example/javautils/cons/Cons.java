758fee19d9582e6299675489fd80ccc11625c626
jinzbone <buptzj@bupt.edu.cn>
2021-03-03 17:37:56 +0800

    Delete JavaUtils2 directory

--- JavaUtils2/src/main/java/com/example/javautils/cons/Cons.java
+++
@@ -1,9 +0,0 @@
-package com;
-
-public class Cons {
-    public static final String TIME_TYPE_YEAR = "本年";
-    public static final String TIME_TYPE_QUARTER = "本季";
-    public static final String TIME_TYPE_MONTH = "本月";
-    public static final String TIME_TYPE_WEEK = "本周";
-    public static final String TIME_TYPE_DAY = "本日";
-}
